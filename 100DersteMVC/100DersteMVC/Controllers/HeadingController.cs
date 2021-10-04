fcfefd4f40a37c9d995a56b5fad2748a1c87d9c3
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2021-10-04 19:32:45 +0300

    Update

--- 100DersteMVC/100DersteMVC/Controllers/HeadingController.cs
+++ 100DersteMVC/100DersteMVC/Controllers/HeadingController.cs
@@ -13,6 +13,7 @@ namespace _100DersteMVC.Controllers
     {
         HeadingManager headingManager = new HeadingManager(new EFHeadingDal());
         CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
+        WriterManager writerManager = new WriterManager(new EFWriterDal());
 
         public ActionResult Index()
         {
@@ -29,6 +30,13 @@ namespace _100DersteMVC.Controllers
                                                       Value = x.CategoryID.ToString()
                                                   }).ToList();
             ViewBag.ValueCategoryDropdown = valueCategory;
+            List<SelectListItem> valueWriter = (from x in writerManager.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName+" "+x.WriterSurName,
+                                                    Value = x.WriterID.ToString()
+                                                }).ToList();
+            ViewBag.ValueWriterDropdown = valueWriter;
             return View();
         }
         [HttpPost]
@@ -38,5 +46,36 @@ namespace _100DersteMVC.Controllers
             headingManager.HeadingAdd(p);
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valueCategory = (from x in categoryManager.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valueCategory;
+
+            var headingValue = headingManager.GetByID(id);
+            return View(headingValue);
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            headingManager.HeadingUpdate(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteHeading(int id)
+        {
+            var headingValue = headingManager.GetByID(id);
+            headingValue.HeadingStatus = false;
+            headingManager.HeadingDelete(headingValue);
+            return RedirectToAction("Index");
+        }
+
+       
     }
 }
